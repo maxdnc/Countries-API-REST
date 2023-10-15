61e3dd6eec161ebf2dc60e9d95c18be88a6a36ac
Maxime Danic <maxime.danic@gmail.com>
2023-10-15 23:03:03 +0200

    ♻️  Add SearchForm and FilterForm to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,12 @@
 // component
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-
-// Icon
-import { Search } from 'lucide-react';
+import FilterForm from "@/components/FilterForm";
+import SearchForm from "@/components/SearchForm";
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input
-          type="text"
-          placeholder="Search for a country..."
-          className="shadow-md"
-        />
-        <Button type="submit" variant="outline" className="shadow ">
-          <span>
-            <Search size={19} />
-          </span>
-          <span className="sr-only">Search</span>
-        </Button>
-      </div>
+    <div className="flex flex-col gap-10">
+      <SearchForm />
+      <FilterForm />
     </div>
   );
 }
