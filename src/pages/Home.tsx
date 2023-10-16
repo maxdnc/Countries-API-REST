a3541cb9528813fd52553f9b995851b7f218f502
Maxime Danic <maxime.danic@gmail.com>
2023-10-16 14:27:17 +0200

    🚧 Add list from all countries

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,36 @@
-// component
+// react query hook
+import { useGetAllCountries } from "@/hooks/useData";
+
+// components
 import CountryCard from "@/components/CountryCard";
 import FilterForm from "@/components/FilterForm";
 import SearchForm from "@/components/SearchForm";
 
+// types
+import { CountryResponseType } from "@/types";
+
 export default function Home() {
+  const { data, isLoading } = useGetAllCountries();
+
   return (
     <div className="flex flex-col gap-10">
       <SearchForm />
       <FilterForm />
+      {isLoading && <p>loading...</p>}
 
-      <CountryCard
-        name="String"
-        population="string"
-        region="string"
-        capital="string"
-      />
+      <ul className="flex flex-col items-center gap-12 py-4">
+        {data?.map((country: CountryResponseType) => (
+          <li key={country.name.official}>
+            <CountryCard
+              name={country.name.official}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+              flag={country.flags.svg}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
