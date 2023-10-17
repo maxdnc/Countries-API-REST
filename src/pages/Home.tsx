9cd8685a92ece159532b03e9afd79a72391066c6
Maxime Danic <maxime.danic@gmail.com>
2023-10-17 23:40:06 +0200

    🚧 Refactor Home in Progress

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,22 @@
 import CountriesList from "@/components/CountriesList";
 import FilterForm from "@/components/FilterForm";
 import SearchForm from "@/components/SearchForm";
+// hook
+import { useFilteredCountries } from "../hooks/useFilteredCountries";
+import { useState } from "react";
+// type
+import { RegionType } from "@/types";
 
 export default function Home() {
+  const [selectedRegion, setSelectedRegion] = useState<RegionType>("Asia");
+
+  const { countries, isLoading } = useFilteredCountries(selectedRegion);
+
   return (
     <div className="flex flex-col gap-10">
       <SearchForm />
       <FilterForm />
-      <CountriesList />
+      <CountriesList data={countries} isLoading={isLoading} />
     </div>
   );
 }
