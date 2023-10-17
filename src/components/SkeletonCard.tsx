f0e931ba6db202406ee57cca925869b1cd387ac6
Maxime Danic <maxime.danic@gmail.com>
2023-10-17 22:11:17 +0200

    ✨ Add skeleton when loading

---
+++ src/components/SkeletonCard.tsx
@@ -0,0 +1,32 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+export default function SkeletonCard() {
+  return (
+    <>
+      <Card className="group relative mx-auto w-full max-w-xs overflow-hidden shadow-lg transition-transform hover:scale-[1.025]">
+        <CardHeader>
+          <Skeleton className="aspect-[133/80]  rounded-none object-cover" />
+        </CardHeader>
+
+        <CardContent className="py-4">
+          <CardTitle className="py-5">
+            <Skeleton className="h-8 w-full" />
+          </CardTitle>
+          <div className="flex flex-col gap-2 ">
+            <Skeleton className="h-5 w-4/5" />
+            <Skeleton className="h-5 w-3/5" />
+            <Skeleton className="h-5 w-5/6" />
+          </div>
+        </CardContent>
+        <CardFooter className="py-4" />
+      </Card>
+    </>
+  );
+}
