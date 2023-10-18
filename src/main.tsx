b46c7594ba5902487d6bec1798ded7ce9f90075d
Maxime Danic <maxime.danic@gmail.com>
2023-10-18 15:13:44 +0200

    📦 Add redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,18 @@ import "./index.css";
 // react query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// react redux
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
