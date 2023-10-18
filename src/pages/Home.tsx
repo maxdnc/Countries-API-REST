e01726d48bb9683d7105f029e29eba186d9498b7
Maxime Danic <maxime.danic@gmail.com>
2023-10-18 15:26:42 +0200

    ♻️ Exploting redux store and useFilteredCountries hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,20 +4,18 @@ import FilterForm from "@/components/FilterForm";
 import SearchForm from "@/components/SearchForm";
 // hook
 import { useFilteredCountries } from "../hooks/useFilteredCountries";
-import { useState } from "react";
-// type
-import { RegionType } from "@/types";
+// redux
+import { useAppSelector } from "@/redux/hook";
 
 export default function Home() {
-  const [selectedRegion, setSelectedRegion] = useState<RegionType>("Asia");
-
-  const { countries, isLoading } = useFilteredCountries(selectedRegion);
+  const filteredParameter = useAppSelector((state) => state.filteredParameter);
+  const { data, isLoading } = useFilteredCountries(filteredParameter);
 
   return (
     <div className="flex flex-col gap-10">
       <SearchForm />
       <FilterForm />
-      <CountriesList data={countries} isLoading={isLoading} />
+      <CountriesList data={data} isLoading={isLoading} />
     </div>
   );
 }
