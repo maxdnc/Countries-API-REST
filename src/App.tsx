31d4ec8f8cfbdc3f4d0ef152d8eae1eeb658eede
Maxime Danic <maxime.danic@gmail.com>
2023-10-20 04:03:24 +0200

    ✨ Add Error Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 // react router
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from "react-router-dom";
 // component
-import MainLayout from './layouts/MainLayout';
+import MainLayout from "./layouts/MainLayout";
 // style
-import { ThemeProvider } from './components/theme-provider';
+import { ThemeProvider } from "./components/theme-provider";
 // page
-import Home from './pages/Home';
-import CountryDetails from './pages/CountryDetails';
+import Home from "./pages/Home";
+import CountryDetails from "./pages/CountryDetails";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
-            <Route path="/country/:name" element={<CountryDetails/>} />
+            <Route path="/country/:name" element={<CountryDetails />} />
+            <Route path="*" element={<Error />} />
           </Route>
         </Routes>
       </HashRouter>
