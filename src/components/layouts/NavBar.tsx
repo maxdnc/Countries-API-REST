245de47b7caf8cb69b1a0aa2475af4f717b12495
Maxime Danic <maxime.danic@gmail.com>
2023-10-21 00:17:31 +0200

    ✨ Update NavBar with new route

--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -1,11 +1,32 @@
+// react router
+import { Link, NavLink } from "react-router-dom";
 // component
 import { ModeToggle } from "../ui/mode-toggle";
+import { buttonVariants } from "../ui/button";
+// icon
+import { Heart } from "lucide-react";
 
 export default function NavBar() {
   return (
     <nav className="mb-4 flex items-center justify-between gap-4 border-border bg-background p-6 shadow dark:border-b">
-      <p className="font-bold text-primary">Where is the world ?</p>
-      <ModeToggle />
+      <Link to="/" className="font-bold text-primary">
+        Where is the world ?
+      </Link>
+      <div className="flex gap-4">
+        <NavLink
+          to="/favorite"
+          className={({ isActive }) =>
+            `${buttonVariants({
+              variant: "outline",
+              className: isActive && "bg-secondary",
+            })}`
+          }
+        >
+          <Heart size={18} strokeWidth={2.5} color="#F337E6" />
+        </NavLink>
+
+        <ModeToggle />
+      </div>
     </nav>
   );
 }
