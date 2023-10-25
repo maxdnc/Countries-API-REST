9211c5d0dae4133e7f983cf25699fe28778c9852
Maxime Danic <maxime.danic@gmail.com>
2023-10-25 20:12:29 +0300

    🚧 Add delete button

--- src/pages/FavoriteCountries.tsx
+++ src/pages/FavoriteCountries.tsx
@@ -1,10 +1,12 @@
-import { useAppSelector } from "@/redux/hook";
+import { Button } from "@/components/ui/button";
+import { removeFavorite } from "@/redux/features/favoriteCountriesSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 export default function FavoriteCountries() {
   const favoriteCountries = useAppSelector(
     (state) => state.favorites.countries,
   );
-  console.log(favoriteCountries);
+  const dispatch = useAppDispatch();
 
   if (favoriteCountries.length === 0) {
     return <p>no favorite country yet</p>;
@@ -13,7 +15,16 @@ export default function FavoriteCountries() {
   return (
     <div className="flex flex-col gap-3">
       {favoriteCountries.map((country) => {
-        return <p key={country.name.official}>{country.name.common}</p>;
+        return (
+          <p key={country.name.official}>
+            {country.name.common}{" "}
+            <Button
+              onClick={() => dispatch(removeFavorite(country.name.official))}
+            >
+              Delete
+            </Button>
+          </p>
+        );
       })}
     </div>
   );
