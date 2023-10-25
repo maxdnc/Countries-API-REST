ee56813755925e0a0987b55ca52581846ea6ee52
Maxime Danic <maxime.danic@gmail.com>
2023-10-25 23:31:41 +0300

    ♻️ Extract FavoriteButton

--- src/components/home/CountriesList/CountryCard.tsx
+++ src/components/home/CountriesList/CountryCard.tsx
@@ -1,4 +1,8 @@
+// react router
 import { Link } from "react-router-dom";
+// component
+import InfoItem from "@/components/ui/info-item";
+import FavoriteButton from "@/components/ui/FavoriteButton";
 import {
   Card,
   CardContent,
@@ -7,22 +11,11 @@ import {
   CardTitle,
 } from "../../ui/card";
 //icon
-import { ArrowUpRight, Heart } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 // helper
 import { formatNumberWithCommas } from "@/helper/math";
-import InfoItem from "@/components/ui/info-item";
 // type
 import type { CountryCardType } from "../../../types";
-import { Button } from "@/components/ui/button";
-// hook
-import { useToast } from "@/components/ui/use-toast";
-import { useCountryDetails } from "@/hooks/useCountryDetails";
-// redux
-import { useAppDispatch, useAppSelector } from "@/redux/hook";
-import {
-  addFavorite,
-  removeFavorite,
-} from "@/redux/features/favoriteCountriesSlice";
 
 export default function CountryCard({
   name,
@@ -31,42 +24,6 @@ export default function CountryCard({
   capital,
   flag,
 }: CountryCardType) {
-  const { toast } = useToast();
-  const { data: country } = useCountryDetails(name);
-  const dispatch = useAppDispatch();
-  const favoriteCountries = useAppSelector(
-    (state) => state.favorites.countries,
-  );
-  const countryData = country?.[0];
-  const isInFavorite = favoriteCountries.some(
-    (favorite) => favorite.name.official === countryData?.name.official,
-  );
-
-  const handleClick = () => {
-    if (country) {
-      const countryData = country[0];
-      const isAlreadyInFavorite = favoriteCountries.some(
-        (favorite) => favorite.name.official === countryData.name.official,
-      );
-      if (!isAlreadyInFavorite) {
-        dispatch(addFavorite(countryData));
-
-        toast({
-          className: "bg-secondary",
-          description: `${name} added to favorites countries list !`,
-        });
-      } else {
-        dispatch(removeFavorite(countryData.name.official));
-        toast({
-          className: "bg-destructive",
-          description: `${name} deleted from your favorites countries list`,
-        });
-      }
-    } else {
-      console.error("Country data is undefined. Cannot add to favorites.");
-    }
-  };
-
   return (
     <>
       <Card className="group relative h-[450px] max-w-xs overflow-hidden shadow-lg  ">
@@ -102,18 +59,10 @@ export default function CountryCard({
           </div>
         </CardContent>
         <CardFooter className="px-5 py-4"></CardFooter>
-        <Button
-          variant="secondary"
-          className={`absolute bottom-4 right-4 z-50 transition  hover:bg-fuchsia-600 active:bg-primary-foreground  ${
-            isInFavorite ? "bg-fuchsia-500 hover:bg-fuchsia-600" : ""
-          }`}
-          onClick={handleClick}
-        >
-          <Heart size={18} strokeWidth={2.5} />
-          <span className="sr-only ">
-            {isInFavorite ? "Delete from favorite" : "Add to favorite"}
-          </span>
-        </Button>
+        <FavoriteButton
+          countryName={name}
+          className={"absolute bottom-4 right-4"}
+        />
       </Card>
     </>
   );
