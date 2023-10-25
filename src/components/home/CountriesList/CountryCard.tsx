2f0cb5baf93125d7ca7c4f2696b218118afbdff5
Maxime Danic <maxime.danic@gmail.com>
2023-10-25 20:10:35 +0300

    🚧 Update handleClick to Delete country on second click

--- src/components/home/CountriesList/CountryCard.tsx
+++ src/components/home/CountriesList/CountryCard.tsx
@@ -7,7 +7,7 @@ import {
   CardTitle,
 } from "../../ui/card";
 //icon
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Heart } from "lucide-react";
 // helper
 import { formatNumberWithCommas } from "@/helper/math";
 import InfoItem from "@/components/ui/info-item";
@@ -19,7 +19,10 @@ import { useToast } from "@/components/ui/use-toast";
 import { useCountryDetails } from "@/hooks/useCountryDetails";
 // redux
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
-import { addFavorite } from "@/redux/features/favoriteCountriesSlice";
+import {
+  addFavorite,
+  removeFavorite,
+} from "@/redux/features/favoriteCountriesSlice";
 
 export default function CountryCard({
   name,
@@ -34,6 +37,10 @@ export default function CountryCard({
   const favoriteCountries = useAppSelector(
     (state) => state.favorites.countries,
   );
+  const countryData = country?.[0];
+  const isInFavorite = favoriteCountries.some(
+    (favorite) => favorite.name.official === countryData?.name.official,
+  );
 
   const handleClick = () => {
     if (country) {
@@ -42,17 +49,17 @@ export default function CountryCard({
         (favorite) => favorite.name.official === countryData.name.official,
       );
       if (!isAlreadyInFavorite) {
-        console.log(typeof country);
         dispatch(addFavorite(countryData));
 
         toast({
           className: "bg-secondary",
-          description: `${name} added to favorites !`,
+          description: `${name} added to favorites countries list !`,
         });
       } else {
+        dispatch(removeFavorite(countryData.name.official));
         toast({
           className: "bg-destructive",
-          description: `${name} is already in your favorites.`,
+          description: `${name} deleted from your favorites countries list`,
         });
       }
     } else {
@@ -94,15 +101,19 @@ export default function CountryCard({
             <InfoItem label="Capital" value={capital} />
           </div>
         </CardContent>
-        <CardFooter className="px-5 py-4">
-          <Button
-            variant="secondary"
-            className="z-50 active:bg-primary-foreground"
-            onClick={handleClick}
-          >
-            Favorite
-          </Button>
-        </CardFooter>
+        <CardFooter className="px-5 py-4"></CardFooter>
+        <Button
+          variant="secondary"
+          className={`absolute bottom-4 right-4 z-50 transition  hover:bg-fuchsia-600 active:bg-primary-foreground  ${
+            isInFavorite ? "bg-fuchsia-500 hover:bg-fuchsia-600" : ""
+          }`}
+          onClick={handleClick}
+        >
+          <Heart size={18} strokeWidth={2.5} />
+          <span className="sr-only ">
+            {isInFavorite ? "Delete from favorite" : "Add to favorite"}
+          </span>
+        </Button>
       </Card>
     </>
   );
