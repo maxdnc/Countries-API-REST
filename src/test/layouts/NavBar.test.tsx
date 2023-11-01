f9e2c31fc3cd01fa2be8c8175dcbedd315a5cfa3
Maxime Danic <maxime.danic@gmail.com>
2023-11-01 22:33:28 +0200

    ✅ Add test for NavBar

--- src/test/layouts/NavBar.test.tsx
+++ src/test/layouts/NavBar.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-
 import { describe, it } from "vitest";
 import { MemoryRouter } from "react-router-dom";
 import NavBar from "@/components/layouts/NavBar";
