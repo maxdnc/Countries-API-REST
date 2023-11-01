2300e12611891f99fff2827e5b9c36fd94b200ad
Maxime Danic <maxime.danic@gmail.com>
2023-11-01 14:58:46 +0200

    🚧 Update vite config for test

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+/// <reference types="vitest"/>
+/// <reference types="vite/client"/>
+
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/test/setupTests.ts"],
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
